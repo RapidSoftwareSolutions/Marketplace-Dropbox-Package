0099f0f3f6b624f2f3761fbb7dc87c8a6ca1511d
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-06-16 12:22:23 +0300

    webhook fixed

--- src/routes/webhookEvent.php
+++ src/routes/webhookEvent.php
@@ -1,39 +1,42 @@
 <?php
-$app->get('/api/Dropbox/webhookEvent', function ($request, $response, $args) {
+$app->group('/api/Dropbox/webhookEvent', function () {
 
-    $challenge = $request->getQueryParams()['challenge'];
-    $body[] = $challenge;
-    $client = new GuzzleHttp\Client();
-    $resp = $client->request('POST', 'http://d7c2294c.ngrok.io', [
-        'json' => $body
-    ]);
+    $this->get('', function ($request, $response, $args) {
 
-    return $challenge;
+        $challenge = $request->getQueryParams()['challenge'];
+        $body[] = $challenge;
+        $client = new GuzzleHttp\Client();
+        $resp = $client->request('POST', 'http://d7c2294c.ngrok.io', [
+            'json' => $body
+        ]);
 
-});
+        return $challenge;
 
-$app->post('/api/Dropbox/webhookEvent', function ($request, $response, $args) {
-    $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, []);
-    if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
-        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
-    } else {
-        $post_data = $validateRes;
-    }
+    });
 
-    $client = new GuzzleHttp\Client();
-    $resp = $client->request('POST', 'http://d7c2294c.ngrok.io', [
-        'json' => $post_data
-    ]);
+    $this->post('', function ($request, $response, $args) {
+        $checkRequest = $this->validation;
+        $validateRes = $checkRequest->validate($request, []);
+        if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
+            return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
+        } else {
+            $post_data = $validateRes;
+        }
 
-    $reply = [
-        "http_resp" => "",
-        "client_msg" => $post_data['args']['body'],
-        "params" => $post_data['args']['params']
-    ];
+        $client = new GuzzleHttp\Client();
+        $resp = $client->request('POST', 'http://d7c2294c.ngrok.io', [
+            'json' => $post_data
+        ]);
 
-    $result['callback'] = 'success';
-    $result['contextWrites']['to'] = $reply;
-    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
+        $reply = [
+            "http_resp" => "",
+            "client_msg" => $post_data['args']['body'],
+            "params" => $post_data['args']['params']
+        ];
 
+        $result['callback'] = 'success';
+        $result['contextWrites']['to'] = $reply;
+        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
+
+    });
 });
