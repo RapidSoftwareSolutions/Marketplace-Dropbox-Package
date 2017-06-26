681930bf444874348d33c6f6832218f496684592
ivz-dev <zakutynsky@yahoo.com>
2017-06-26 19:27:13 +0300

    test

--- src/routes/downloadFile.php
+++ src/routes/downloadFile.php
@@ -41,7 +41,7 @@ $app->post('/api/Dropbox/downloadFile', function ($request, $response, $args) {
                     ],
                     [
                         "name" => "file",
-                        "filename" => bin2hex(random_bytes(5)) .'.'. explode('.', $post_data['args']['filePath'])[count(explode('.', $post_data['args']['filePath']))-1],
+                        "filename" => md5($post_data['args']['filePath']),
                         "contents" => $responseBody
                     ]
                 ]
